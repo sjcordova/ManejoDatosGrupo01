c3963bb3e021fa816561b28080193da28e66248d
sjcordova <santiago.cordova.paredes@udla.edu.ec>
2024-11-27 08:01:03 -0500

    CLase APIs

---
+++ ManejoDatosGrupo01/Models/EstudianteAPI.cs
@@ -0,0 +1,32 @@
+﻿using Android.Locations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejoDatosGrupo01.Models
+{
+    public class EstudianteAPI
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public Address Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<string> Courses { get; set; }
+        public double Gpa { get; set; }
+        public string Image { get; set; }
+    }
+
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ ManejoDatosGrupo01/Repositories/EstudianteUDLAPorAPIsRepository.cs
@@ -0,0 +1,50 @@
+﻿using ManejoDatosGrupo01.Interfaces;
+using ManejoDatosGrupo01.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejoDatosGrupo01.Repositories
+{
+    public class EstudianteUDLAPorAPIsRepository : IEstudianteUDLARepository
+    {
+
+        public string _urlEndpoint = "https://freetestapi.com/api/v1/students";
+
+        public bool ActualizarEstudianteUDLA(EstudianteUDLA estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CrearEstudianteUDLA(EstudianteUDLA estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EstudianteUDLA DevuelveEstudianteUDLA(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EstudianteUDLA> DevuelveListadoEstudiantes()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var response = httpClient.GetAsync(_urlEndpoint).Result;
+                var json_data = response.Content.ReadAsStringAsync().Result;
+
+                List<EstudianteAPI> estudiantesAPI = JsonConvert.DeserializeObject<List<EstudianteAPI>>(json_data);
+            }
+
+            
+        }
+
+        public bool EliminarEstudianteUDLA(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
